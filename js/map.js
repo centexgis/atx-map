25dfb70f55a2c832b7c37e18d0724936a56dc4f9
John Haney <jwhaney@users.noreply.github.com>
2017-09-28 15:57:42 -0500

    Add files via upload

--- js/map.js
+++ js/map.js
@@ -1,10 +1,10 @@
 //create var bounds
-var northWest = L.latLng(30.52, -98.03),
-    southEast = L.latLng(30.00, -97.50),
+var northWest = L.latLng(30.404, -97.815),
+    southEast = L.latLng(30.189, -97.660),
     bounds = L.latLngBounds(northWest, southEast);
 
 //create map - zoom to var bounds
-var map = L.map('map');
+var map = L.map('map', { dragging: !L.Browser.mobile });
 map.fitBounds(bounds);
 
 //load osm basemap tiles
@@ -20,6 +20,6 @@ L.easyButton('fa-globe fa-lg', function(){
 //load adelberts marker location and popup info
 var marker = L.marker([30.27384, -97.74058], {title: "Austin, TX"});
 
-var markerContent = "<strong>CenTexGIS</strong>" + "<br>" + "Austin, TX";
+var markerContent = "<strong>CENTEXGIS</strong><br>" + "Austin, TX";
 
 marker.bindPopup(markerContent).addTo(map);
